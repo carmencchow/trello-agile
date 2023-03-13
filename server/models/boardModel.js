13133d4140b768e5b87750125410b67b02d4a000
Carmen Chow <carmendevelops@gmail.com>
2023-03-13 17:42:06 -0400

    make boardSchema

---
+++ server/models/boardModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const boardSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Board', boardSchema);
