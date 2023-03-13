63fb3ff42c129f1f8c2a3dddea4fe40071f2c98d
funkee <xxtrm98@protonmail.com>
2023-03-13 14:52:25 -0700

    added test css styling, installed front-end dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Trello Clone</h1>
+        <h1 className="css-test">Trello Clone</h1>
       </header>
     </div>
   );
