ccada806e6ccda748df2819d42967dd30402dc01
Ezra Godde <ezraalexandergodde@gmail.com>
2023-03-14 13:05:49 -0700

    put trello card in trello list, card needs button to add text, no work on header yet

--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,9 +1,11 @@
+import TrelloCard from './TrelloCard';
 import './TrelloList.css';
 
 const TrelloList = ({title}) => {
   return (
     <div className='list-container'>
       <h3>{title}</h3>
+      <TrelloCard />
       </div>
   )
 };
