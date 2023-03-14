64a301c16622d5ad1833d6ea2c9a6a102290f46e
funkee <xxtrm98@protonmail.com>
2023-03-13 17:32:01 -0700

    changed server, commented mongodb out

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5001;
-const cors = require('cors');
+const cors = require("cors");
 
 // Express app
 const app = express();
@@ -11,18 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  res.header('Access-Control-Allow-Origin', '*');
+  console.log(req.path, req.method);
+  res.header("Access-Control-Allow-Origin", "*");
   next();
-})
+});
+
+app.get("/test", (req, res) => {
+  res.send("Testing the server.");
+});
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`connected to db and listening to port ${PORT}`);
-    })
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`connected to db and listening to port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+app.listen(PORT, () => {
+  console.log(`Connected and listening to port ${PORT}`);
+});
