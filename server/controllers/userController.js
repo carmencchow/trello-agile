e0704a4ddc2a856bcfd302b84dfceec40814cbec
funkee <xxtrm98@protonmail.com>
2023-03-14 11:59:08 -0700

    added endpoint to load users into db

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,3 @@
-const User = require("../models/userModel");
 const mongoose = require("mongoose");
 
 // REGISTER user
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const User = require("../models/userModel");
 const { login, logout, register } = require("../controllers/userController");
 
 const router = express.Router();
 // Load Users Data. Only hit once or duplicates will ensue. Duplicates...
 router.get("/get-users", (req, res) => {
-  res.send("Users will load here.");
+  try {
+    let user = new User({
+      name: "JohnnyAppleseed",
+      email: "JohnnyApple@gmail.com",
+      password: "pa$$w0rd",
+      boards: [],
+    });
+    user.save();
+    res.send("User loaded to database.");
+  } catch (err) {
+    console.log(err);
+    res.send("Error generating Data");
+  }
 });
 // ROUTES '/api/user'
 router.post("/register", register);
