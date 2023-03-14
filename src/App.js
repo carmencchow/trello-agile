3e595554e32f69f93f13fe96984d7c3e1cf74b64
Ezra Godde <ezraalexandergodde@gmail.com>
2023-03-14 12:25:36 -0700

    building out rough components, made TrelloList, need to decide on a css framework for cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import TrelloList from "./components/TrelloList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TrelloList title={'To Do'} />
     </div>
   );
 }
---
+++ src/components/TrelloList.js
@@ -0,0 +1,11 @@
+import './TrelloList.css';
+
+const TrelloList = ({title}) => {
+  return (
+    <div className='list-container'>
+      <h3>{title}</h3>
+      </div>
+  )
+};
+
+export default TrelloList;
