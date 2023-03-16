23e47b77d6a9ed6319fe28b83e26380fb5b2a81e
funkee <xxtrm98@protonmail.com>
2023-03-16 11:14:02 -0700

    basic authentication added, thank the heavens

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const User = require("../models/userModel");
-const { login, logout, register } = require("../controllers/userController");
-
+// const { login, logout, register } = require("../controllers/userController");
+// const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const auth = require("../middleware/auth");
 // Load Users Data. Only hit once or duplicates will ensue. Duplicates...
 router.get("/get-users", (req, res) => {
   try {
@@ -19,9 +22,107 @@ router.get("/get-users", (req, res) => {
     res.send("Error generating Data");
   }
 });
+
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    let user = await User.findOne({
+      email,
+    });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    user = new User({
+      username,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      "randomString",
+      {
+        expiresIn: 10000,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json({
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Error in saving");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({
+      email,
+    });
+    if (!user)
+      return res.status(400).json({
+        message: "User does not exist",
+      });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Incorrect Password!",
+      });
+    }
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      "randomString",
+      {
+        expiresIn: 3600,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json({
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    // request.user is getting fetched from Middleware after token authentication
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (e) {
+    res.send({ message: "Error in Fetching user" });
+  }
+});
+
 // ROUTES '/api/user'
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
+// router.post("/register", register);
+// router.post("/login", login);
+// router.post("/logout", logout);
 
 module.exports = router;
