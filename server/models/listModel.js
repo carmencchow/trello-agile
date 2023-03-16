e0a33f51020f47bdd3a4f9438fbd62998f3870f4
funkee <xxtrm98@protonmail.com>
2023-03-16 14:32:32 -0700

    added dummy data for testing

--- server/models/listModel.js
+++ server/models/listModel.js
@@ -1,24 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listSchema = new Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  cards: [
-    {
+const listSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    cards: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "card",
+      },
+    ],
+
+    // If we want to move the list to another board:
+    board: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'card',
-    }
-  ],
-
-  // If we want to move the list to another board:
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'board',
+      ref: "board",
+    },
   },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('List', listSchema);
+module.exports = mongoose.model("List", listSchema);
