a69f568904f9c5468885eea1aba1df216bb9a93e
funkee <xxtrm98@protonmail.com>
2023-03-19 14:10:13 -0700

    preliminary login functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import "./Header.css";
+import Login from "./Login";
 import Navbar from "./Navbar";
 
 function Header() {
   return (
     <div>
       <Navbar />
-      <div className="header">
+      {/* <div className="header">
         Trello header wide that spread the top of the whole screen!
-      </div>
+      </div> */}
+      <Login />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return <div></div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userInfo, setUserInfo] = useState("");
+  const [token, setToken] = useState("");
+  // const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const challenge = { email: email, password: password };
+    axios
+      .post("http://localhost:5000/api/user/login", challenge)
+      .then((res) => setToken(res.data.token));
+
+    setEmail("");
+    setPassword("");
+  };
+
+  useEffect(() => {
+    const getUserProfile = async () => {
+      axios
+        .get("http://localhost:5000/api/user/me", {
+          headers: {
+            token: `${token}`,
+          },
+        })
+        .then((res) => setUserInfo(res.data))
+        .catch((error) => console.log(error));
+
+      // .then((res) => console.log(res));
+    };
+    if (token) {
+      getUserProfile();
+    }
+  }, [token]);
+
+  // const getUserProfile = async () => {
+  //   axios
+  //     .get("http://localhost:5000/api/user/me", {
+  //       headers: {
+  //         token: `${token}`,
+  //       },
+  //     })
+  //     .then((res) => setUserInfo(res.data))
+  //     .catch((error) => console.log(error));
+
+  //   // .then((res) => console.log(res));
+  // };
+
+  // const userPanel = userInfo.map((item, index) => {
+  //   return (
+  //     <div key={index}>
+  //       <h1>Welcome {item.username}</h1>
+  //     </div>
+  //   );
+  // });
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Log in</button>
+      </form>
+      {/* user profile if user exists */}
+      <div>
+        {userInfo && (
+          <div>
+            Hi, {userInfo.username}!<h4>Your boards:</h4>
+            {userInfo.boards.length > 0 ? (
+              userInfo.boards.map((board, index) => {
+                return (
+                  <div key={index}>
+                    <h5>{board.title}</h5>
+                  </div>
+                );
+              })
+            ) : (
+              <p>"You don't have any boards, yet.</p>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
