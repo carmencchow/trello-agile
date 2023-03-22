7c9ac8af15945bf7a0df614a9819f656acc74bde
Carmen Chow <carmendevelops@gmail.com>
2023-03-22 17:02:26 -0400

    remove drag items, add input field for new card

--- src/components/Board.js
+++ src/components/Board.js
@@ -25,68 +25,13 @@ const Board = () => {
     message: ''
   });
 
-  const [list, setLists] = useState([])
-  // const [list, setLists] = useState([
-  //   board: '',
-  //   cards: [{
-  //     labels: [],
-  //     members: [],
-  //     owner: [],
-  //     parentList: [],
-  //     _id: '',
-  //     title: '',
-  //     name: '',
-  //   }],
-  // ])
-
-// For drag and drop feature
-  const [cards, setCards] = useState([])
-  const dragItem = useRef()
-  const dragOverItem = useRef()
- 
- // ********* TEST CARDS (DRAG & DROP WORKS): **********
-
-  // const [item, setItem] = useState(['test_card 1', 'test_card 2', 'test_card 3', 'test_card 4', 'test_card 5', 'test_card 6', 'test_card 7', 'test_card 8', 'test_card 9'])
-  
-  // const drop = (e) => {
-  //   const copyItems = [...item];
-  //   const dragItemContent = copyItems[dragItem.current];
-  //   copyItems.splice(dragItem.current, 1);
-  //   copyItems.splice(dragOverItem.current, 0, dragItemContent);
-  //   dragItem.current = null;
-  //   dragOverItem.current = null;
-  //   setItem(copyItems);
-  // };
-
-  const dragStart = (e, position) => {
-    dragItem.current = position
-    console.log('Dragging item: ', e.target.innerHTML);
-  }
-
-  const dragEnter = (e, position) => {
-    dragOverItem.current = position
-    console.log('Dragging over item: ', e.target.innerHTML)
-  }
-
-// ************************************************
-
-// ********** TRELLO CARDS (DRAG NOT WORKING) ******:
-  const dropTrelloCards = (e) => {
-    const copyCards = [...cards];
-    const dragCardContent = copyCards[dragItem.current];
-    copyCards.splice(dragItem.current, 1);
-    copyCards.splice(dragOverItem.current, 0, dragCardContent);
-    dragItem.current = null;
-    dragOverItem.current = null;
-    setCards(copyCards);
-  };
+  // const [list, setLists] = useState([])
 
   const getBoard = async () => {
     const res = await axios.get(`http://localhost:5000/api/board/${id}`)
     setBoard(res.data.board)
     console.log('Board info:', res.data)
     console.log('List info:', res.data.board.lists)
-    // setLists(res.data.board.board)
     }
 
   useEffect (() => {
@@ -98,27 +43,6 @@ const Board = () => {
       <Navbar/>
       <h3>{board.board && board.board.title}</h3>
 
-
-  {/* ********* TEST CARDS (DRAG & DROP WORKS): *********/}
-      {/* <div className="test-container">
-          {item && item.map((x, idx) => (
-          <div className="test-draggable-container">
-            <div className="test-draggable-item"
-              onDragStart={(e) => dragStart(e, idx)}
-              onDragEnter={(e) => dragEnter(e, idx)}
-              onDragOver={(e) => e.preventDefault()}
-              onDragEnd={drop}
-              key={idx}
-              draggable>
-              {x}
-            </div>
-          </div>
-        ))}
-      </div> */}
-
-  {/* *****************************************************/}
-
-  {/* ***************** TRELLO CARD ***********************/}
       <div className="container">
         {board.lists && board.lists.map((list, idx) => {
           return (
@@ -133,54 +57,19 @@ const Board = () => {
                 </span>
               </span>
 
-              <div 
-                className="cards"
-                onDragStart={(e) => dragStart(e, idx)}
-                onDragEnter={(e) => dragEnter(e, idx)}
-                onDragOver={(e) => e.preventDefault()}
-                onDragEnd={dropTrelloCards}
-                // key={idx}
-                draggable>
-                  trello_card:{list.cards} 1
-              </div>
-
-              <div 
-                className="cards"
-                onDragStart={(e) => dragStart(e, idx)}
-                onDragEnter={(e) => dragEnter(e, idx)}
-                onDragOver={(e) => e.preventDefault()}
-                onDragEnd={dropTrelloCards}
-                // key={idx}
-                draggable>
-                  trello_card:{list.cards} 2
-              </div>
-
-              <div 
-                className="cards"
-                onDragStart={(e) => dragStart(e, idx)}
-                onDragEnter={(e) => dragEnter(e, idx)}
-                onDragOver={(e) => e.preventDefault()}
-                onDragEnd={dropTrelloCards}
-                // key={idx}
-                draggable>
-                  trello_card:{list.cards} 3
-              </div> 
+              <div className="cards">trello_card:{list.cards} 1</div>
+              <div className="cards">trello_card:{list.cards} 2</div>
+              <div className="cards">trello_card:{list.cards} 3</div> 
               
               <div>
                 <span className="add-card" onClick={() => setOpenNewCard(true)}>+ Add a card
-                <AddCard 
-                  open={openNewCard} 
-                  // onClose={() => openNewCard(false)}
-                  />
+                <AddCard open={openNewCard}/>
                 </span>
               </div>
             </div>
-
-          )
-        })}
-      </div>
-      
-
+            )
+          })}
+        </div>
       </div>
       )
     }
