a4d9efbd4fbf95815b96dd47d57151c9ae19e1ef
funkee <xxtrm98@protonmail.com>
2023-03-22 10:10:18 -0700

    added auth and data cleanup to delete card endpoint

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -67,10 +67,17 @@ const addMembers = async (req, res) => {};
 // DELETE a card (working)
 const deleteCard = async (req, res) => {
   try {
-    const cardId = await Card.findOneAndDelete({ _id: req.params.id });
-    if (!cardId) {
+    const card = await Card.findByIdAndDelete({ _id: req.params.id });
+
+    if (!card) {
       return res.status(404).send("Card not found");
     }
+    const list = await List.findById(card.parentList);
+
+    list.cards = list.cards.filter(
+      (cardId) => cardId.toString() !== card._id.toString()
+    );
+    await list.save();
     return res.send("Card deleted");
   } catch (err) {
     return res.status(500).send({ message: "Error deleting card" });
@@ -102,15 +109,15 @@ const createCard = async (req, res) => {
 
 // UPDATE card details (working)
 const updateCardName = async (req, res) => {
-  const { id, name } = req.body;
+  const { title } = req.body;
   try {
-    const card = await Card.findOneAndUpdate(
-      { id: id },
-      { name },
-      { new: true }
-    );
+    const card = await Card.findById({ _id: req.params.id });
+    if (!card) {
+      res.status(404).send("Card not found");
+    }
+    card.title = title;
     await card.save();
-    console.log(card._id, card.name);
+    // console.log(card._id, card.name);
     res.status(200).send({ message: "Card name updated", card });
   } catch (err) {
     console.log(err);
