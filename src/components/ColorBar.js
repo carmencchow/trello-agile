684f0b04e14290e55efd19b859482bcff514d79f
Carmen Chow <carmendevelops@gmail.com>
2023-03-24 23:24:46 -0400

    resolving issues from merge

--- src/components/ColorBar.js
+++ src/components/ColorBar.js
@@ -2,25 +2,39 @@ import React, { useState } from 'react'
 import './ColorBar.css'
 
 const ColorBar = () => {
-  const [bgColor, setBgColor] = useState("yellow")
+  const [bgColor, setBgColor] = useState
+  
+  const colors = [
+    "#FF0000", 
+    "#FFA500", 
+    "#dada07", 
+    "#90EE90", 
+    "#87cefa", 
+    "#dda0dd", 
+    "#ffc0cb", 
+    "#deb887"
+  ]
 
-  const colorChange = (e) => {
-    console.log('change color to classname', e.target.value)
-    setBgColor(e.target.value)
-  }
+  // const colorChange = (e) => {
+  //   console.log('change color to classname', e.target.value)
+  //   setBgColor(e.target.value)
+  // }
 
   return (
     <div className="color-container">
       <div className="color-row">
-        <span className="red" onClick={colorChange}></span>
-        <span className="orange"></span>
-        <span className="yellow"></span>
-        <span className="green"></span>
-        <span className="blue"></span>
-        <span className="purple"></span>
-        <span className="pink"></span>
-        <span className="brown"></span>
+        {colors.map((color, idx) => {
+          return (
+            <span>{color}</span>
+          )        
+        })}
+
       </div>
+
+      <div className="results-container">
+        <span className="results"></span>
+      </div>
+    
     </div>
   )
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,118 +1,107 @@
-import React, { useState, useEffect } from 'react'
-import { BsThreeDots } from 'react-icons/bs'
-import AddCard from '../components/AddCard'
-import SaveCardBtn from './SaveCardBtn'
-import CardPopup from './CardPopup'
-import { GrEdit } from 'react-icons/gr'
-import './List.css'
-import { GrClose } from 'react-icons/gr'
-import "./AddCard.css";
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import React, { useState } from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { GrClose, GrEdit } from "react-icons/gr";
+// import ListModal from "./ActivityModal";
+import AddCard from "../components/AddCard";
+import SaveCardBtn from "./SaveCardBtn";
+// import CancelCard from "./CancelCard";
+import CardPopup from "./CardPopup";
+import "./List.css";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
-const List = ({ name, cards, close, onClose }) => {
-  const [openModal, setOpenModal] = useState(false)
-  const [openNewCard, setOpenNewCard] = useState(false)
-  const [input, setInput] = useState('');
-  const { id } = useParams();
+const List = ({ name, cards, _id }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [openNewCard, setOpenNewCard] = useState(false);
 
-  const [list, setList] = useState('');
+  const toggleModal = () => {
+    setOpenModal(!openModal);
+  };
 
-  // if (!open) return null;
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
 
-  const openCard = () => {
-    console.log('opening card')
-    setOpenModal(!openModal)
-  }
-
-  const handleInput = (e) => {
-    setInput(e.target.value)
-  }  
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      throw new Error("No token found in localStorage");
+    if (!destination) {
+      return;
     }
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    const getList = async () => {
-      const res = await axios.get(`http://localhost:5000/api/list/${id}`);
-      // console.log(list);
-      setList(res.data.board);
-      console.log("Board info:", res.data);
-      // console.log("List info:", res.data.board.lists[0].cards);
-    };
 
-    getList();
-  }, [id]);
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    console.log(`Moving card ${draggableId} from ${source.droppableId} to ${destination.droppableId}`);
+
+    //update the state to move card
+  };
 
   return (
     <div className="list">
-      <div className="list-header">
+      <span className="list-header">
         <p className="list-name">{name}</p>
-        <span className="dots"><BsThreeDots/></span>
-      </div>
+        <span className="dots" onClick={() => toggleModal()}>
+          <BsThreeDots />
+        </span>
+      </span>
 
-      {cards.map((card) => (
-        <div key={card._id} 
-          className="cards"
-          onClick={() => openCard()}>
-          {card.title}
-          <span className="icon">{<GrEdit/>}</span>
-        </div>
-      ))}
+      <Droppable droppableId={_id}>
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {cards.map((card, index) => (
+              <Draggable
+                key={card._id}
+                draggableId={card._id}
+                index={index}
+              >
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    key={card._id}
+                    className="cards"
+                  >
+                    {card.title}
+                    <span className="icon">{<GrEdit/>}</span>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
 
       <CardPopup 
         open={openModal}
-        onClose={() => setOpenModal(false)}
-        name={name}
-        cards={cards}
-      />
-
-      {/* <div></div> */}
-      {/* <div className="input-field">
-    
-      <AddCard 
-        open={openNewCard}
-        onClose={() => setOpenNewCard(false)}/> */}
+        onClose={() => setOpenModal(false)}/>
       
-        { openNewCard ? (
-          <div className="input-container">
-            {/* <div className="new-card"> */}
-            <div onClick={onClose} className="new-card">
-              <input type="text" 
-                className="card" 
-                value={input}
-                placeholder="Enter a title for this card..."
-                onChange={handleInput}/>
+      {/* <div>
+        {openModal && <ListModal toggleModal={toggleModal} />}
+      </div> */}
 
-                <div className="card-btns">
-                  <SaveCardBtn/>  
-                  <button className="cancel" onClick={onClose}><GrClose/></button>
-                </div>
+      <div className="input-field">
+        <AddCard open={openNewCard} />
 
-                {/* <button className="add-card" 
-                  onClick={() => { setOpenNewCard(true) }}>
-                    Add a card</button> : 
-                <div className="card-btns">
-                    <SaveCardBtn/>  
-                </div> */}
-        
-              </div>
-            </div> 
-    
-            ) : (
+        {!openNewCard ? (
+          <button
+            className="add-card"
+            onClick={() => {
+              setOpenNewCard(true);
+            }}
+          >
+            Add a card
+          </button>
+        ) : (
+          <div className="card-btns">
+            <SaveCardBtn />
+            {/* <CancelCard /> */}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-            <div className="input-field">
-              <button className="add-card" 
-                onClick={() => { setOpenNewCard(true) }}>
-                  Add a card
-              </button>
-            </div>
-          )}
-        </div>
-      )
-    }
-
-  export default List
-
+export default List;
