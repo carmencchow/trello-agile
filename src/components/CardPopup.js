de8f001a48030760f460dabf7e2614170a577885
funkee <xxtrm98@protonmail.com>
2023-03-25 19:52:03 -0700

    creation of delete button, functionality coming soon.

--- src/components/CardPopup.js
+++ src/components/CardPopup.js
@@ -1,45 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import { GrFormClose } from 'react-icons/gr'
-import './CardPopup.css'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { GrFormClose } from "react-icons/gr";
+import "./CardPopup.css";
+import axios from "axios";
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const CardPopup = ({ open, onClose, _id }) => {
-  const [color, setColor] = useState("green")
-  const [cardData, setCardData] = useState(null)
-  
+  const [color, setColor] = useState("green");
+  const [cardData, setCardData] = useState(null);
+
   const fetchCardInfo = async (id) => {
-    const res = await axios.get(`http://localhost:5000/api/card/${id}`)    
-    console.log('Card details: ', res.data)
-    setCardData(res.data)
-  }
+    const res = await axios.get(`http://localhost:5000/api/card/${id}`);
+    console.log("Card details: ", res.data);
+    setCardData(res.data);
+  };
 
-  useEffect(() =>{
-    fetchCardInfo(_id)
-  }, [])
+  const handleDelete = () => {
+    alert("This card will be deleted!");
+  };
 
-  if(!open || cardData ===null) return null;
+  useEffect(() => {
+    fetchCardInfo(_id);
+  }, [_id]);
+
+  if (!open || cardData === null) return null;
 
   return (
     <div className="card-background">
       <div className="card-popup">
-        <div>   
-          <div className="card-popup-heading" style={{ backgroundColor: color}}>
-            <h2>{cardData.card.title} </h2>  
-            <div className="right-side">      
-              <GrFormClose className="close" onClick={onClose}/>
+        <div>
+          <div
+            className="card-popup-heading"
+            style={{ backgroundColor: color }}
+          >
+            <h2>{cardData.card.title} </h2>
+            <div className="right-side">
+              <GrFormClose className="close" onClick={onClose} />
             </div>
           </div>
 
           <div className="card-content">
-          <p>Click to change  your color</p>
+            <p>Click to change your color</p>
 
-            <div className="color-row"> 
+            <div className="color-row">
               <span className="red" onClick={() => setColor("red")}></span>
-              <span className="orange" onClick={() => setColor("orange")}></span>
-              <span className="yellow" onClick={() => setColor("yellow")}></span>
+              <span
+                className="orange"
+                onClick={() => setColor("orange")}
+              ></span>
+              <span
+                className="yellow"
+                onClick={() => setColor("yellow")}
+              ></span>
               <span className="green" onClick={() => setColor("green")}></span>
               <span className="blue" onClick={() => setColor("blue")}></span>
-              <span className="purple" onClick={() => setColor("purple")}></span>
+              <span
+                className="purple"
+                onClick={() => setColor("purple")}
+              ></span>
               <span className="pink" onClick={() => setColor("pink")}></span>
               <span className="brown" onClick={() => setColor("brown")}></span>
             </div>
@@ -48,12 +66,20 @@ const CardPopup = ({ open, onClose, _id }) => {
             <p>Activity</p>
             <p>Archive</p>
             <p>Members{cardData.card.members}</p>
+            <div>
+              <Button
+                variant="outlined"
+                startIcon={<DeleteIcon />}
+                onClick={handleDelete}
+              >
+                Delete
+              </Button>
+            </div>
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardPopup
+export default CardPopup;
