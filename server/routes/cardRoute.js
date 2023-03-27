b9982015440f53cecbb1a48415984829be0f8969
funkee <xxtrm98@protonmail.com>
2023-03-26 19:32:25 -0700

    made change to boardRoute

--- server/routes/cardRoute.js
+++ server/routes/cardRoute.js
@@ -23,11 +23,11 @@ const router = express.Router();
 // router.delete("/:id", auth, deleteCard);
 // router.put("/:id", auth, updateCardName);
 
-router.get("/", getCards);
-router.get("/:id", getCard);
-router.post("/", createCard);
-router.delete("/:id", deleteCard);
-router.put("/:id", updateCardName);
+router.get("/", auth, getCards);
+router.get("/:id", auth, getCard);
+router.post("/", auth, createCard);
+router.delete("/:id", auth, deleteCard);
+router.put("/:id", auth, updateCardName);
 
 // Are we going to use these endpoints for the MVP?
 router.get("/members/:id", auth, getMembersFromCard);
--- src/components/CardPopup.js
+++ src/components/CardPopup.js
@@ -1,22 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { GrFormClose } from "react-icons/gr";
-import io from "socket.io-client";
-import { FiEdit2 } from 'react-icons/fi'
+// import io from "socket.io-client";
+import { FiEdit2 } from "react-icons/fi";
 import "./CardPopup.css";
 import DeleteCard from "./DeleteCard";
 import EditCard from "./EditCard";
-import ArchiveCard from './ArchiveCard';
+import ArchiveCard from "./ArchiveCard";
 
-
-const socket = io.connect("http://localhost:5000");
-
-  
+// const socket = io.connect("http://localhost:5000");
 
 const CardPopup = ({ open, onClose, id, handleFetchData, listId }) => {
   const [color, setColor] = useState("green");
   const [cardData, setCardData] = useState(null);
-  const [messageReceived, setMessageReceived] = useState("");
+  // const [messageReceived, setMessageReceived] = useState("");
   const [openInput, setOpenInput] = useState(false);
 
   const colorArr = [
@@ -30,18 +27,17 @@ const CardPopup = ({ open, onClose, id, handleFetchData, listId }) => {
     "brown",
   ];
 
+  // const sendMessage = () => {
+  //   socket.emit("send_message", { message: "hello" });
+  // };
 
-  const sendMessage = () => {
-    socket.emit("send_message", { message: "hello" });
-  };
-
-  useEffect(() => {
-    const socket = io.connect("http://localhost:5000");
-    socket.on("receive_message", (data) => {
-      console.log(data.message);
-      setMessageReceived(data.message);
-    });
-  }, []);
+  // useEffect(() => {
+  //   const socket = io.connect("http://localhost:5000");
+  //   socket.on("receive_message", (data) => {
+  //     console.log(data.message);
+  //     setMessageReceived(data.message);
+  //   });
+  // }, []);
 
   // (1) GET card by id
 
@@ -56,7 +52,6 @@ const CardPopup = ({ open, onClose, id, handleFetchData, listId }) => {
     getCard(id);
   }, [id]);
 
-
   // useEffect(() => {
   //   socket.on("receive_message", (data) => {
   //     console.log(data);
@@ -69,14 +64,16 @@ const CardPopup = ({ open, onClose, id, handleFetchData, listId }) => {
   //   });
   //   handleFetchData();
 
-
   if (!open || cardData === null) return null;
 
   return (
     <div className="card-background">
       <div className="card-popup">
         <div>
-          <div className="card-popup-heading" style={{ backgroundColor: color }}>
+          <div
+            className="card-popup-heading"
+            style={{ backgroundColor: color }}
+          >
             <h2>{cardData.card.title} </h2>
             <div className="right-side">
               <GrFormClose className="close" onClick={onClose} />
@@ -84,9 +81,9 @@ const CardPopup = ({ open, onClose, id, handleFetchData, listId }) => {
           </div>
 
           <div className="card-content">
-          <p>Click a square to change the background color</p>
+            <p>Click a square to change the background color</p>
 
-            <div className="color-row"> 
+            <div className="color-row">
               {colorArr.map((color) => {
                 return (
                   <span
@@ -101,60 +98,61 @@ const CardPopup = ({ open, onClose, id, handleFetchData, listId }) => {
               <p>Label: {cardData.card.labels}</p>
               <p>Activity: </p>
 
-              <p>{messageReceived}</p>
+              {/* <p>{messageReceived}</p>
               <input placeholder="send message"></input>
-              <button onClick={sendMessage}>send message</button>
-              <p className="archive" onClick={handleArchive}>
+              <button onClick={sendMessage}>send message</button> */}
+              {/* <p className="archive" onClick={handleArchive}>
                 Archive this card
-              </p>
+              </p> */}
               <p className="edit">
                 <EditCard />
-
-
+              </p>
               <p className="archive">
                 <ArchiveCard
                   handleFetchData={handleFetchData}
                   id={id}
                   onClose={onClose}
                 />
-
               </p>
 
-            <div className="input-field">
-              <EditCard
-                open={openInput}
-                listId={listId}
-                id={id}
-                handleFetchData={handleFetchData}
-                onClose={onClose}
-              />
+              <div className="input-field">
+                <EditCard
+                  open={openInput}
+                  listId={listId}
+                  id={id}
+                  handleFetchData={handleFetchData}
+                  onClose={onClose}
+                />
 
                 {!openInput ? (
-                  <h4 className="edit-card" onClick={() => {
+                  <h4
+                    className="edit-card"
+                    onClick={() => {
                       setOpenInput(true);
-                    }}> Edit this card <FiEdit2/></h4>
-                  ) : (
-                    <div></div>
-                  )}
+                    }}
+                  >
+                    {" "}
+                    Edit this card <FiEdit2 />
+                  </h4>
+                ) : (
+                  <div></div>
+                )}
               </div>
             </div>
 
-              <p className="delete">
-                <DeleteCard
-                  handleFetchData={handleFetchData}
-                  id={id}
-                  onClose={onClose}
-                />
-              </p>
+            <p className="delete">
+              <DeleteCard
+                handleFetchData={handleFetchData}
+                id={id}
+                onClose={onClose}
+              />
+            </p>
 
-              <p>View members{cardData.card.members}</p>
-  
-            </div>
+            <p>View members{cardData.card.members}</p>
           </div>
-     
+        </div>
       </div>
     </div>
-
   );
 };
 
